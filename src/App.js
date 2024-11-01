78f8067c2cee5a2e635207fa345cac3066bab058
SCAN07\Rolf <rh@websoftart.ch>
2024-11-01 14:01:39 +0100

    Tests_01

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
 import { Grid } from "@mui/material";
 import { CardCenter } from "./components";
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { alpha, styled } from '@mui/material/styles';
+import { pink } from '@mui/material/colors';
+import Switch from '@mui/material/Switch';
+import TextField from '@mui/material/TextField';
+
+const PinkSwitch = styled(Switch)(({ theme }) => ({
+  '& .MuiSwitch-switchBase.Mui-checked': {
+    color: pink[600],
+    '&:hover': {
+      backgroundColor: alpha(pink[600], theme.palette.action.hoverOpacity),
+    },
+  },
+  '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': {
+    backgroundColor: pink[600],
+  },
+}));
+
+
+
+const label = { inputProps: { 'aria-label': 'Color switch demo' } };
 
 const App = ()=>{
   return(
@@ -9,7 +42,8 @@ const App = ()=>{
     sx={{
       justifyContent: "space-between",
       alignItems: "center",
-      height: window.innerHeight
+      // when spacing between grid rows is needed
+      // height: window.innerHeight
     }}
 >
   <Grid item>
@@ -17,6 +51,84 @@ const App = ()=>{
   </Grid>
   <Grid item>
     <CardCenter/>
+  </Grid>
+  <Grid item>
+  <Stack direction="row" spacing={2}>
+      <Button variant="contained">Contained</Button>
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
+      <Button variant="contained" href="#contained-buttons">
+        Link
+      </Button>
+    </Stack>
+  </Grid>
+  <Grid>
+    <FormGroup>
+      <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+      <FormControlLabel required control={<Checkbox />} label="Required" />
+      <FormControlLabel disabled control={<Checkbox />} label="Disabled" />
+    </FormGroup>
+  </Grid>
+  <Grid>
+    <div><FormLabel>RadioGroup from Rolf (TODO: how to add new lines with FormLabel?)</FormLabel></div>
+    <FormControl>
+      <FormLabel id="demo-row-radio-buttons-group-label">Gender</FormLabel>
+      <RadioGroup
+        row
+        aria-labelledby="demo-row-radio-buttons-group-label"
+        name="row-radio-buttons-group"
+      >
+        <FormControlLabel value="female" control={<Radio />} label="Female" />
+        <FormControlLabel value="male" control={<Radio />} label="Male" />
+        <FormControlLabel value="other" control={<Radio />} label="Other" />
+        <FormControlLabel
+          value="disabled"
+          disabled
+          control={<Radio />}
+          label="other"
+        />
+      </RadioGroup>
+    </FormControl>    
+  </Grid>
+  <Grid>
+    <FormControl fullWidth>
+      <FormLabel>TODO: how do declare age? handleChange?</FormLabel>
+    <InputLabel id="demo-simple-select-label">Age</InputLabel>
+    <Select
+      labelId="demo-simple-select-label"
+      id="demo-simple-select"
+      /* value={age} */
+      label="Age"
+      /* onChange={handleChange} */
+    >
+      <MenuItem value={10}>Ten</MenuItem>
+      <MenuItem value={20}>Twenty</MenuItem>
+      <MenuItem value={30}>Thirty</MenuItem>
+    </Select>
+    </FormControl>    
+  </Grid>
+  <Grid>
+    <div>
+      <Switch {...label} defaultChecked />
+      <Switch {...label} defaultChecked color="secondary" />
+      <Switch {...label} defaultChecked color="warning" />
+      <Switch {...label} defaultChecked color="default" />
+      <PinkSwitch {...label} defaultChecked />
+    </div>
+  </Grid>
+  <Grid>
+    <FormLabel>TODO: how do declare function "CheckName"? Javascript?</FormLabel>
+    <div>
+        <TextField
+          error
+          id="outlined-error-helper-text"
+          label="Enter a name here"
+          defaultValue="Hello World"
+          helperText="This name is not correct"
+          /* onChange={CheckName()} */
+        />
+      </div>    
   </Grid>
 </Grid>
   )
